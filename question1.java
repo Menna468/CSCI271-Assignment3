ed68d8f09cc9a9d0b360a360575b6ba90bb56bf9
Menna Ibrahim <ibrahimm2@winthrop.edu>
2026-02-23 14:38:50 -0500

    finished first question

--- question1.java
+++ question1.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class question1 {
     public static void main (String[] args) {
 
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String S = input.nextLine();
+        int result = sum(S);
+        System.out.println("Sum = " + result);
+
+        input.close();
+    }
+
+    public static int sum(String n) {        
+        if (n.isEmpty()) {
+            return 0;
+        }
+
+        return 1 + sum(n.substring(1));   
+
     }
 }
+
+//The running time of this function is O(n^2)
---
+++ question2.java
@@ -0,0 +1,1 @@
+//set S and C to either lower case or both uppercase
